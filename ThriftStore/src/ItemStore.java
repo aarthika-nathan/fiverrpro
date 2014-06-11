af2609f3d640335e97bb808ef64ebf7ea6f9cccf
Aarthika Arunthavanathan <aarthika.nathan.11@cse.mrt.ac.lk>
2014-06-11 12:32:17 +0530

    Added a new class Item store

---
+++ ThriftStore/src/ItemStore.java
@@ -0,0 +1,33 @@
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+
+public class ItemStore extends JFrame {
+
+	public ItemStore() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ItemStore(GraphicsConfiguration arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ItemStore(String arg0) throws HeadlessException {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ItemStore(String arg0, GraphicsConfiguration arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
